bcd74b18f9caf1ed052b101e87c4c8f7c48e44a1
ti250 <ti250@cam.ac.uk>
2021-11-15 11:07:16 +0100

    Update according to patch described in #31

--- src/dawg.pyx
+++ src/dawg.pyx
@@ -350,7 +350,7 @@ cdef class CompletionDAWG(DAWG):
 
         return completer.Next()
 
-    cpdef bytes tobytes(self) except +:
+    cpdef bytes tobytes(self):
         """
         Return raw DAWG content as bytes.
         """
